437ee60a3c6157f280cd9926a14fac7237e9da74
Scott Thiessen <scott@movingobjects.io>
2021-05-08 14:37:07 -0700

    Add initial plants data

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,11 +3,21 @@ import React from 'react';
 
 import style from './index.module.scss';
 
+import speciesData from '~/src/data/species';
+import plantsData from '~/src/data/plants';
+
 const App = () => {
 
   return (
     <div className={style.wrap}>
       <h1>Plants Watering Log</h1>
+
+      <h3>Species</h3>
+      <pre>{JSON.stringify(speciesData, null, 2)}</pre>
+
+      <h3>Plants</h3>
+      <pre>{JSON.stringify(plantsData, null, 2)}</pre>
+
     </div>
   )
 
