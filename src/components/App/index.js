9b710c9a664e2e4c83f334fdc7e7f907c896669e
Scott Thiessen <scott@movingobjects.io>
2021-05-08 14:36:55 -0700

    Add initial dump of files

---
+++ src/components/App/index.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+
+import style from './index.module.scss';
+
+const App = () => {
+
+  return (
+    <div className={style.wrap}>
+      <h1>Plants Watering Log</h1>
+    </div>
+  )
+
+}
+
+export default App;
