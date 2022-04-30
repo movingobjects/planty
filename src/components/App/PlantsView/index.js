ad0abb49a89fd89862f4014b7b5d2fe74d2adfd3
Scott Thiessen <scott@movingobjects.io>
2022-04-30 10:40:21 -0700

    Add image storage

--- src/components/App/PlantsView/index.js
+++ src/components/App/PlantsView/index.js
@@ -1,12 +1,135 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { API, Storage } from 'aws-amplify';
+import { listPlants } from 'graphql/queries';
+import {
+  createPlant as createPlantMutation,
+  deletePlant as deletePlantMutation
+} from 'graphql/mutations';
 
 import style from './index.module.scss';
 
+const emptyFormState = {
+  name: ''
+}
+
 function PlantsView() {
+
+  const [ plants, setPlants ] = useState([]);
+  const [ formData, setFormData ] = useState(emptyFormState);
+
+  useEffect(() => {
+    fetchPlants();
+  }, []);
+
+  async function fetchPlants() {
+    const apiData = await API.graphql({
+      query: listPlants
+    });
+    const plantsFromAPI = apiData.data.listPlants.items;
+    await Promise.all(plantsFromAPI.map(async plant => {
+      if (plant.image) {
+        const image = await Storage.get(plant.image);
+        plant.image = image;
+      }
+      return plant;
+    }))
+    setPlants(apiData.data.listPlants.items);
+  }
+
+  async function addPlant() {
+    if (!formData.name) return;
+    await API.graphql({
+      query: createPlantMutation,
+      variables: {
+        input: formData
+      }
+    });
+    if (formData.image) {
+      const image = await Storage.get(formData.image);
+      formData.image = image;
+    }
+    setPlants([
+      ...plants,
+      formData
+    ]);
+    setFormData(emptyFormState);
+  }
+
+  async function deletePlant({ id }) {
+    const nextPlants = plants.filter(plant => plant.id !== id);
+    setPlants(nextPlants);
+    await API.graphql({
+      query: deletePlantMutation,
+      variables: {
+        input: {
+          id
+        }
+      }
+    });
+  }
+
+  async function onFileSelect(e) {
+    const file = e?.target?.files?.[0];
+    if (!file) return;
+    setFormData({
+      ...formData,
+      image: file.name
+    });
+    await Storage.put(file.name, file);
+    fetchPlants();
+  }
 
   return (
     <div className={style.wrap}>
+
       <h2>PlantsView</h2>
+
+      <div>
+        <h3>Add plant</h3>
+
+        <input
+          onChange={(e) => {
+            setFormData({
+              ...formData,
+              'name': e.target.value
+            })
+          }}
+          placeholder="Plant name"
+          value={formData.name} />
+
+        <input
+          type="file"
+          onChange={onFileSelect} />
+
+        <button onClick={addPlant}>Add Plant</button>
+      </div>
+
+      <div>
+        <h3>Plants</h3>
+        <ul>
+          {plants.map(plant => (
+            <li key={plant.id || plant.name}>
+              <p>
+                {!!plant.image && (
+                  <img
+                    alt={plant.name}
+                    src={plant.image}
+                    style={{
+                      width: 100
+                    }} />
+                )}
+                {plant.name}
+                &nbsp;
+                <button
+                  onClick={() => deletePlant(plant)}>
+                  &times;
+                </button>
+              </p>
+            </li>
+          ))}
+        </ul>
+      </div>
+
     </div>
   );
 }
