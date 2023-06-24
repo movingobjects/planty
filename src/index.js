bd3886a085d26b3418b77d1f00f21656f7dd0b78
Scott Thiessen <scott@movingobjects.io>
2023-06-24 08:27:27 -0700

    Update amplify

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Amplify from 'aws-amplify';
+import { Amplify } from 'aws-amplify';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from 'components/App';
